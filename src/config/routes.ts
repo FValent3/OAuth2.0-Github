d85eed51b269536718bb3d7819a414d17d1c5d7a
Anya <fpvrodrigues@gmail.com>
2022-12-20 13:48:13 -0300

    feat: serve static files

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,6 +1,7 @@
-import { Express, Router } from 'express'
+import { Express, Router, static as _static } from 'express'
 
 export default (app: Express): void => {
   const router = Router()
+  app.use(_static(path.join(_dirname, '/public')))
   app.use('/api', router)
 }
