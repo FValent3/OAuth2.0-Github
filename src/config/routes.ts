6c000e3b2a91315313aa3bc1755f9ce2fd230d8d
Anya <fpvrodrigues@gmail.com>
2022-12-20 13:54:23 -0300

    fix: small typo

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,7 +1,8 @@
 import { Express, Router, static as _static } from 'express'
+import path from 'path'
 
 export default (app: Express): void => {
   const router = Router()
-  app.use(_static(path.join(_dirname, '/public')))
+  app.use(_static(path.join(__dirname, '/public')))
   app.use('/api', router)
 }
